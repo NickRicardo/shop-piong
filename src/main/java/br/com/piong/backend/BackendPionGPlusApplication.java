21077e827dbd00527ddc3f1109db61318da977ee
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-11-26 17:10:42 -0300

    first commit

---
+++ src/main/java/br/com/piong/backend/BackendPionGPlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.piong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPionGPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPionGPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/piong/backend/BackendPionGPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.piong.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPionGPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
